53529f7350824a08288ae9d856af27de7dc84fd9
admin <krivalcevich@gmail.com>
2022-10-22 13:44:51 +0300

    add les 7.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  title = "instagram";
-  text=''
-
-
-  changeTitleHandler = () => {
-    this.title = 'it-incubator'
-  };
-
-  changeInputHandler = (event:Event)=> {
-    console.log((event.currentTarget as HTMLInputElement).value)
-    this.text = (event.currentTarget as HTMLInputElement).value
-  }
+  text = 'Hello'
 
 
 }
